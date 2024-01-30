de551fc4a607bd92e525fa33c7d3823b5e6ca726
UnderKid21 <underkid21@gmail.com>
2024-01-30 20:47:32 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -70,6 +70,23 @@ const refs = {
 };
 
 refs.gallery.innerHTML = createMarcup(images);
+function createMarcup(arr) {
+    return arr.map(
+        ({ preview, original, description }) =>
+          `<li class="gallery-item">
+    <a class="gallery-link" href="${original}">
+      <img
+        class="gallery-image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`,
+      )
+      .join('');
+  }
+  
 
 refs.gallery.addEventListener('click', onGalleryClick);
 
@@ -100,23 +117,6 @@ function onGalleryClick(event) {
   instance.show();
 }
 
-function createMarcup(arr) {
-  return arr
-    .map(
-      ({ preview, original, description }) =>
-        `<li class="gallery-item">
-  <a class="gallery-link" href="${original}">
-    <img
-      class="gallery-image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`,
-    )
-    .join('');
-}
 
 function onModalClose(event) {
   if (event.code === 'Escape') {
